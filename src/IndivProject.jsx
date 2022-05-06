1c91c2f794254d2f26c468e12caa406878c4e7f8
jodii <Jodi_tan@mymail.sutd.edu.sg>
2022-05-06 16:18:06 +0800

    modal popup for add expense

--- src/IndivProject.jsx
+++ src/IndivProject.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Component, useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Alert, List, Divider, Skeleton, Typography, Button, Row, Col} from 'antd';
+import { Alert, List, Divider, Skeleton, Typography, Button, Row, Col, Modal, Input} from 'antd';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { EditOutlined, DeleteOutlined, PlusOutlined} from '@ant-design/icons';
 
@@ -39,69 +39,115 @@ const IndivProject = (props) => {
         }
     ]);
     const [err_1, setError] = useState(false)
+    const [name, setName] = useState("")
+    const [amount, setAmount] = useState("")
 
     
 
     useEffect(() => {
-        //loadExpenses()
+        loadExpenses()
     }, [])
 
+    function filter_projectid(event) {
+        return event.project_id == props.project_id;
+    }
 
-    // const loadExpenses = async () => {
-    //     await axios.get(`/project-get`, {}, {params: {
-    //         project: props.project_id,
-    //     }})
-    //     .then(res => {
-    //         setExp(res.data)
-    //         console.log("success")
-    //     })
-    //     .catch(error => {
-    //         setError(true)
-    //         console.log(error)
-    //     })
-    // }
+    const loadExpenses = async () => {
+        //get whole list of expense
+        await axios.get(`/expense`, {}, {
+        //     params: {
+        //     project_id: 2 //props.project_id,
+        // }
+        })
+        .then(res => {
+            //filter based on props.project_id
+            var filtered = res.data.filter(filter_projectid);
+            setExp(filtered)
+            console.log(res)
+            console.log("success")
+        })
+        .catch(error => {
+            setError(true)
+            console.log(error)
+        })
+    }
 
     const editExpenses = async (id) => {
         console.log("edit", id)
-        // await axios.post(`/project-update`, {}, {params: {
-        //     project: id,
-        // }})
-        // .then(res => {
-        //     console.log("success")
-        // })
-        // .catch(error => {
-        //     setError(true)
-        //     console.log(error)
-        // })
+        await axios.post(`/expense-update`, {}, {params: {
+            expense: id,
+        }})
+        .then(res => {
+            console.log("success")
+        })
+        .catch(error => {
+            setError(true)
+            console.log(error)
+        })
     }
 
-    const deleteExpenses = () => { 
+    const deleteExpenses = async (id) => { 
         console.log("delete")
-        // await axios.delete(`/project-delete`, {}, {params: {
-        //     project: id,
-        // }})
-        // .then(res => {
-        //     console.log("success")
-        // })
-        // .catch(error => {
-        //     setError(true)
-        //     console.log(error)
-        // })
+        await axios.delete(`/expense_delete`, {}, {params: {
+            expense: id,
+        }})
+        .then(res => {
+            console.log("success")
+        })
+        .catch(error => {
+            setError(true)
+            console.log(error)
+        })
     }
 
-    const addExpenses = () => { 
-        console.log("add")
-        // await axios.post(`/project-add`, {}, {params: {
-        //     project: id,
-        // }})
-        // .then(res => {
-        //     console.log("success")
-        // })
-        // .catch(error => {
-        //     setError(true)
-        //     console.log(error)
-        // })
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = async () => {
+        //call api
+        console.log(name)
+        console.log(amount)
+        await axios.post(`/expense_add`, {}, {params: {
+            project_id: null, //props.project_id,
+            name: name,
+            amount: amount,
+        }})
+        .then(res => {
+            console.log("success")
+        })
+        .catch(error => {
+            setError(true)
+            console.log(error)
+        })
+
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    const onChange_name = (e) => {
+        setName(e.target.value)
     }
+
+    const onChange_amt= (e) => {
+        setAmount(e.target.value)
+    }
+
+
+
+
+    const addExpenses = async () => { 
+        //open modal
+        showModal()
+    }
+
+
 
 
     
@@ -110,6 +156,9 @@ const IndivProject = (props) => {
 
         {err_1 ? 
         <Alert message="Something's Wrong..." type="warning" />: null }
+
+        <h1>Project No: 1 {/* props.project_id  */}</h1>
+        <h1>Total Number of Expenses: {exp.length!==0 ? exp.length : 0}</h1>
 
         {/* <div className="list-group">
 
@@ -160,6 +209,19 @@ const IndivProject = (props) => {
 
         )}): null} 
         </div>*/}
+
+    <Modal title="add an expense to this project" visible={isModalVisible} closable={false}
+    footer={[
+        <Button key="back" onClick={handleCancel}>
+          Return
+        </Button>,
+        <Button key="submit" type="primary" onClick={handleOk}>
+          Submit
+        </Button>,
+    ]}>
+        <Input value={name} onChange={onChange_name} addonBefore="Name: " />
+        <Input value={amount} onChange={onChange_amt} addonBefore="Amount: " />
+      </Modal>
 
         <List
             itemLayout="horizontal"
